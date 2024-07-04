b9969f6f0f32d9a1b42022efa3f65c8c882c7fb1
Moe <muhtasim1023@gmail.com>
2024-07-04 17:58:32 -0400

    inital commit

---
+++ mag_cal.py
@@ -0,0 +1,34 @@
+import typing
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.optimize as opt
+
+def generate_data() -> np.ndarray:
+    #Generate a set of random unit vectors
+    
+    
+    return
+
+
+def main():
+    """
+        Generate random Magnetometer data.
+        In reality you would grab real sensor
+        data and import the data into python but the 
+        calibration works similarly
+    """
+
+    generate_data()
+
+
+    return
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
