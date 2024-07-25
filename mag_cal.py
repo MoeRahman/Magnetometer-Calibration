f78aa447a5eda3e4534d96733125874a82c2ed59
Moe <muhtasim1023@gmail.com>
2024-07-25 10:45:38 -0400

    kode styling

--- mag_cal.py
+++ mag_cal.py
@@ -5,50 +5,50 @@ import numpy as np
 from scipy.optimize import least_squares
 
 def generate_uncalibrated_data() -> np.ndarray:
-    
+
     #Generate a set of random unit vectors
     phi = np.random.uniform(0, np.pi*2, 500)
     theta = np.random.uniform(0, np.pi*2, 500)
     points = np.array([np.sin( theta ) * np.cos( phi ),
               np.sin( theta ) * np.sin( phi ),
-              np.cos( theta ), 
+              np.cos( theta ),
               np.ones(500)])
-    
+
     randScaleX, randScaleY, randScaleZ = [np.random.uniform(0, 10),
                                           np.random.uniform(0, 10),
                                           np.random.uniform(0, 10)]
-    
+
     offsetX, offsetY, offsetZ = [np.random.uniform(0, 2),
                                  np.random.uniform(0, 2),
                                  np.random.uniform(0, 2)]
-    
-    
+
+
     affine_transform = np.array( [[randScaleX, 0.0, 0.0, offsetX],
                                   [0.0, randScaleY, 0.0, offsetY],
                                   [0.0, 0.0, randScaleZ, offsetZ],
                                   [0.0, 0.0, 0.0, 1]])
-    
+
     biased_data = np.matmul(affine_transform, points)
 
     return points, biased_data
 
-def three_dim_scatter(cal_points:np.ndarray, 
+def three_dim_scatter(cal_points:np.ndarray,
                       ideal_sphere:np.ndarray,
                       uncal_points:np.ndarray) -> None:
-    
+
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     Ux, Uy, Uz = uncal_points[:3]
     Cx, Cy, Cz = cal_points[:3]
     Ix, Iy, Iz = ideal_sphere[:3]
 
-    ax.scatter(Ux, Uy, Uz, marker = 'o', 
+    ax.scatter(Ux, Uy, Uz, marker = 'o',
                label="Uncalibrated Data") #Uncalibrated data
-    ax.scatter(Cx, Cy, Cz, color = 'red', marker = 'o', 
+    ax.scatter(Cx, Cy, Cz, color = 'red', marker = 'o',
                label="Calibrated Data") #Calibrated
-    ax.scatter(Ix, Iy, Iz, color = 'green', marker = '+', 
+    ax.scatter(Ix, Iy, Iz, color = 'green', marker = '+',
                label="Ideal Data") #Ideal
-    
+
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
@@ -58,7 +58,7 @@ def three_dim_scatter(cal_points:np.ndarray,
     rms_error = np.sqrt(np.mean(error**2))
 
     handles, labels = ax.get_legend_handles_labels()
-    handles.append(plt.Line2D([0], [0], color='w', 
+    handles.append(plt.Line2D([0], [0], color='w',
                               label=f'RMS Error: {rms_error:.4f}'))
     ax.legend(handles=handles, loc='upper left')
 
@@ -78,20 +78,20 @@ def unit_circle_fit(points:np.ndarray) -> np.ndarray:
 
         # Reshape affine parameters to 4x4 matrix
         affine_transform = affine_params.reshape((4, 4))
-        transformed_points = apply_affine_transformation(points, 
+        transformed_points = apply_affine_transformation(points,
                                                          affine_transform)
-        
+
         distances = np.linalg.norm(transformed_points, axis=1) - 1
         return np.sum(distances ** 2)
 
-    
+
     # Initialize with the identity affine transformation
     initial_affine_transformation = np.eye(4).flatten()
-    result = least_squares(loss_function, initial_affine_transformation, 
+    result = least_squares(loss_function, initial_affine_transformation,
                       args=(points,))
     optimized_affine_transformation = result.x.reshape((4, 4))
     print(optimized_affine_transformation)
-    
+
     return optimized_affine_transformation
 
 
@@ -99,7 +99,7 @@ def main() -> None:
     """
         Generate random Magnetometer data.
         In reality you would grab real sensor
-        data and import the data into python but the 
+        data and import the data into python but the
         calibration works similarly
     """
 
@@ -109,7 +109,7 @@ def main() -> None:
     calibrated = np.matmul(calibrated_transform, uncalibrated)
 
     three_dim_scatter(calibrated[:3] ,unit_sphere[:3], uncalibrated[:3])
-    
+
 
     return
 
