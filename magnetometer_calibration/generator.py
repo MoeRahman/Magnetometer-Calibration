7337f86e1232694ef852acb463292772f322e4a4
Moe <muhtasim1023@gmail.com>
2026-06-29 14:23:23 -0400

    removed extra indents

--- magnetometer_calibration/generator.py
+++ magnetometer_calibration/generator.py
@@ -24,15 +24,11 @@ class GenerateSensorData():
         true_data = np.array([mag_x, mag_y, mag_z])
 
         #Random Magnetic Biases
-        soft_iron_bias = np.array([[1.0, 0.5, 0.0],
-                                   [0.0, 10.0, 0.0],
-                                   [0.6, 0.0, 1.0]])
-
+        soft_iron_bias = np.array([[1.0, 0.1, 0.0], [0.0, 10.0, 0.0], [0.2, 0.0, 1.0]])
         hard_iron_bias = np.random.uniform(0, 5, (3,1))
 
         #Random Noise Matrix
         noise = np.random.normal(0, 0.1, (3, self.number_of_points))
-
         bias_sensor_data = soft_iron_bias@true_data + hard_iron_bias + noise
 
         return bias_sensor_data
@@ -47,7 +43,7 @@ def scatter_hist(x, y, ax, ax_histx, ax_histy):
     ax.scatter(x, y, s = 0.25)
 
     # now determine nice limits by hand:
-    binwidth = 0.05
+    binwidth = 0.25
     xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
     lim = (int(xymax/binwidth) + 1) * binwidth
 
@@ -63,10 +59,10 @@ def main() -> None:
     X, Y, Z = magnetometer_sensor_data.generate_points()[:3]
 
     plots = [
-    ("X vs Y", X, Y),
-    ("X vs Z", X, Z),
-    ("Y vs Z", Y, Z)
-]
+        ("X vs Y", X, Y),
+        ("X vs Z", X, Z),
+        ("Y vs Z", Y, Z)
+    ]
 
     for title, data_x, data_y in plots:
         fig, axs = plt.subplot_mosaic([['histx', '.'], ['scatter','histy']],
@@ -76,7 +72,7 @@ def main() -> None:
         scatter_hist(data_x, data_y, axs['scatter'], axs['histx'], axs['histy'])
 
     fig = plt.figure()
-    ax = fig.add_subplot(projection='3d', aspect = 'equal')
+    ax = fig.add_subplot(projection='3d')
     ax.scatter(X, Y, Z, s = 1, c = "red")
 
     plt.show()
